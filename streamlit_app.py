8739385338ad7d61e11f91c8adb95b53f26b8c3e
vha22 <havananh.ftu@gmail.com>
2024-06-12 00:04:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-url = 'https://raw.githubusercontent.com/vha22/bank-marketing/main/bank.csv'
+url = 'https://raw.githubusercontent.com/sdrcr74/test/main/bank.csv'
 bank = pd.read_csv(url)
 st.title("Bank Marketing Campaign")
 st.sidebar.title("Sommaire")
